8a610031407d650bd18cc89af5f93ae26fdafb39
Joel Outeiro <joel@outeiro.com>
2025-11-07 23:27:38 +0100

    intial nixos config

---
+++ configuration.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+  [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Madrid";
+
+  i18n.defaultLocale = "gl_ES.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_ES.UTF-8";
+    LC_IDENTIFICATION = "es_ES.UTF-8";
+    LC_MEASUREMENT = "es_ES.UTF-8";
+    LC_MONETARY = "es_ES.UTF-8";
+    LC_NAME = "es_ES.UTF-8";
+    LC_NUMERIC = "es_ES.UTF-8";
+    LC_PAPER = "es_ES.UTF-8";
+    LC_TELEPHONE = "es_ES.UTF-8";
+    LC_TIME = "es_ES.UTF-8";
+  };
+
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    #media-session.enable = true;
+  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  users.users.joel = {
+    isNormalUser = true;
+    description = "joel";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    google-chrome
+    vscode
+    ghostty
+    gh
+    git
+  ];
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  services.openssh.enable = true;
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
