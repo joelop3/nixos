86d02ddb838bc27187f33585379dbb334b261e70
Joel Outeiro <joel@outeiro.com>
2025-11-08 00:43:52 +0100

    Nixos flake setup clean

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.joel = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
           }
         ];
       };
